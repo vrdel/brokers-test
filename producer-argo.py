e9f7bd31333b942e89668916a947d2fbc449761a
Daniel Vrcic <daniel.vrcic@gmail.com>
2016-02-12 11:05:49 +0100

    fixed date timestamp

--- producer-argo.py
+++ producer-argo.py
@@ -15,28 +15,31 @@ def send_msg(args, client, msg):
     else:
         client.send(args.d[0], msg)
 
-def gen_msg(prefix):
+def gen_msg(args):
     yearl = ['2014', '2015', '2016']
     statusl = ['OK', 'WARNING', 'MISSING', 'CRITICAL', 'UKNOWN', 'DOWNTIME']
     monthl = ['%s' % '{:=02}'.format(i) for i in range(1,12)]
-    # monthl = ["%s" % "{:=02}".format(i) for i < 31 i += 1]
     daysl = ['%s' % '{:=02}'.format(i) for i in range(1,31)]
     secl = ['%s' % '{:=02}'.format(i) for i in range(1,60)]
     minutl = ['%s' % '{:=02}'.format(i) for i in range(1,60)]
     hourl = ['%s' % '{:=02}'.format(i) for i in range(1,12)]
 
+    prefix = args.p[0] if args.p else None
+    if prefix:
+        msgd = 'detailsData: %s-%s\n' % (prefix, ''.join(random.sample('abcdefghijklmno', 10)))
+    else:
+        msgd = 'detailsData: %s\n' % ''.join(random.sample('abcdefghijklmno', 10))
+    if args.a:
+        timestamp = 'timestamp: %sT%s:%s:%sZ\n' % (args.a[0], random.choice(hourl), random.choice(minutl), random.choice(secl))
+    else:
+        timestamp = 'timestamp: %s-%s-%sT%s:%s:%sZ\n' % (random.choice(yearl), random.choice(monthl), random.choice(daysl),
+                                                        random.choice(hourl), random.choice(minutl), random.choice(secl),)
     service = 'serviceType: %s\n' % ''.join(random.sample('abcdefghijklmno', 10))
-    timestamp = 'timestamp: %s-%s-%sT%s:%s:%sZ\n' % (random.choice(yearl), random.choice(monthl), random.choice(daysl),
-                                                     random.choice(hourl), random.choice(minutl), random.choice(secl),)
     hostname = 'hostName: %s\n' % ''.join(random.sample('abcdefghijklmno', 10))
     metric = 'metricName: %s\n' % ''.join(random.sample('abcdefghijklmno', 10))
     status = 'metricStatus: %s\n' % random.choice(statusl)
     nagiosh = 'nagios_host: %s\n' % ''.join(random.sample('abcdefghijklmno', 10))
     summaryd = 'summaryData: %s\n' % ''.join(random.sample('abcdefghijklmno', 10))
-    if prefix:
-        msgd = 'detailsData: %s-%s\n' % (prefix, ''.join(random.sample('abcdefghijklmno', 10)))
-    else:
-        msgd = 'detailsData: %s\n' % ''.join(random.sample('abcdefghijklmno', 10))
 
     msg = service + hostname + timestamp + metric + status + nagiosh + summaryd + msgd + 'EOT\n'
     return msg
@@ -45,7 +48,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', nargs=1, required=True, help='destination', metavar='dest')
     parser.add_argument('-e', action='store_true', default=False, help='persistent msgs flag')
-    parser.add_argument('-m', nargs=1, required=True, help='msg', metavar='msg')
+    parser.add_argument('-a', nargs=1, required=False, help='fixed timestamp', metavar='fixed timestamp')
     parser.add_argument('-n', nargs=1, default=False, help='number of msgs', metavar='int')
     parser.add_argument('-p', nargs=1, default=False, help='msg prefix', metavar='msg prefix')
     parser.add_argument('-s', nargs=1, required=True, help='broker', metavar='broker')
@@ -62,17 +65,17 @@ def main():
         i = 0
         if args.n:
             while i < int(args.n[0]):
-                msg = gen_msg(args.p[0] if args.p else None)
+                msg = gen_msg(args)
                 if args.v:
-                    print str(i) + '\n' + '------------------'
+                    print str(i)
                     print msg
                 send_msg(args, client, msg)
                 i += 1
         else:
             while True:
-                msg = gen_msg(args.p[0] if args.p else None)
+                msg = gen_msg(args)
                 if args.v:
-                    print str(i) + '\n' + '------------------'
+                    print str(i)
                     print msg
                 send_msg(args, client, msg)
                 i += 1
